137c3767f236405948bd75a7effc5a2c164e1217
Anna Ostropolets <anna.ostropolets@odysseusinc.com>
2020-11-25 10:36:11 -0500

    Update GetCalendarYearRange.sql

--- inst/sql/sql_server/GetCalendarYearRange.sql
+++ inst/sql/sql_server/GetCalendarYearRange.sql
@@ -5,4 +5,4 @@ SELECT CASE
            THEN YEAR(MIN(observation_period_start_date))
            ELSE 2000 END                        AS start_year,
        YEAR(MAX(observation_period_end_date)) AS end_year
-FROM ohdsi_cumc_deid_2019q2.dbo.observation_period;
+FROM @cdm_database_schema.observation_period;
