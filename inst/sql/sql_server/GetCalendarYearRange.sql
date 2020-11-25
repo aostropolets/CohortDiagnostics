4a7a9e0144a579baf85dc02ee0a005e56093ba46
Anna Ostropolets <anna.ostropolets@odysseusinc.com>
2020-11-25 03:01:42 -0500

    adding case to calendar years

--- inst/sql/sql_server/GetCalendarYearRange.sql
+++ inst/sql/sql_server/GetCalendarYearRange.sql
@@ -1,5 +1,8 @@
 {DEFAULT @cdm_database_schema = CDM_jmdc_v1063.dbo}
 
-SELECT YEAR(MIN(observation_period_start_date)) AS start_year,
-	YEAR(MAX(observation_period_end_date)) AS end_year
-FROM @cdm_database_schema.observation_period;
+SELECT CASE
+         WHEN YEAR(MIN(observation_period_start_date)) >= 2000 -- XXX case for db that go back to 90s
+           THEN YEAR(MIN(observation_period_start_date))
+           ELSE 2000 END                        AS start_year,
+       YEAR(MAX(observation_period_end_date)) AS end_year
+FROM ohdsi_cumc_deid_2019q2.dbo.observation_period;
